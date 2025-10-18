6984053485572e6f0de4e49538fe31cf5030685c
Daniel Lu <accounts@danielclu.com>
2025-10-17 21:43:22 -0700

    clean up maze puzzles

--- src/levels/maze.ts
+++ src/levels/maze.ts
@@ -2,12 +2,12 @@ import { Level } from '../level'
 import { TileMap } from '../tile-map'
 import * as emoji from '../emoji'
 
-type MazeSet = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7
+type MazeSet = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 export class MazeLevel extends Level<MazeSet> {
   constructor(root: HTMLDivElement) {
     super({
       start: TileMap.fromArray<MazeSet>([
-        [1, 1, 3, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 1, 7, 1, 1, 1, 1, 1, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
         [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
@@ -27,7 +27,7 @@ export class MazeLevel extends Level<MazeSet> {
         [1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1],
         [1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1],
         [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-        [1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 3],
+        [1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 8],
         [0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
         [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
         [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -41,16 +41,17 @@ export class MazeLevel extends Level<MazeSet> {
     if (symbol === 0) return emoji.BLUE
     if (symbol === 1) return emoji.RED
     if (symbol === 2) return emoji.YELLOW
-    if (symbol === 3) return emoji.FACE
-    if (symbol === 4) return emoji.LEFT
-    if (symbol === 5) return emoji.UP
-    if (symbol === 6) return emoji.RIGHT
-    return emoji.DOWN
+    if (symbol === 3) return emoji.LEFT
+    if (symbol === 4) return emoji.UP
+    if (symbol === 5) return emoji.RIGHT
+    if (symbol === 6) return emoji.DOWN
+    if (symbol === 7) return emoji.GRIN
+    return emoji.STAR_STRUCK
   }
 
   advance(symbol: MazeSet | null): MazeSet | null {
-    const n = ((symbol ?? -1) + 1) % 9
-    if (n === 8) return null
+    const n = ((symbol ?? -1) + 1) % 10
+    if (n === 9) return null
     return n as MazeSet
   }
 }
